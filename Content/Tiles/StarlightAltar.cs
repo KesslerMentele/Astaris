9b2a7eb545022309335dd5e06d14bddb3c37f1b8
Kessler Mentele <143213858+KesslerMentele@users.noreply.github.com>
2025-01-22 08:35:51 -0800

    Fixed duplicate item drop.

--- Content/Tiles/StarlightAltar.cs
+++ Content/Tiles/StarlightAltar.cs
@@ -18,26 +18,11 @@ public class StarlightAltar : ModTile
         TileID.Sets.DisableSmartCursor[Type] = true;
         
         
-        // TODO: This is wrong
         TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
         TileObjectData.newTile.CoordinateHeights = new[] {16, 16, 16};
         TileObjectData.newTile.CoordinatePaddingFix = new Point16(0, 2);
         TileObjectData.addTile(Type);
         
         AddMapEntry(new Color(224,188,69), CreateMapEntryName());
-    }
-
-    
-    // TODO: FIX THIS
-    public override void KillMultiTile(int i, int j, int frameX, int frameY)
-    {
-        Item.NewItem(
-            new EntitySource_TileBreak(i,j),
-            i * 16,
-            j * 16,
-            48,
-            32,
-            ModContent.ItemType<Items.Placeables.StarlightAltar>()
-        );
     }
 }
